7cdee9752af8680507579d2991526f25a2bdf88a
Nurullo Ismoilov <i.n.ismoilov@gmail.com>
2022-10-17 09:19:13 +0500

    Refactored code

---
+++ backend/tests/test_routes/test_jobs.py
@@ -0,0 +1,39 @@
+import json
+from urllib import response
+
+
+def test_create_job(client):
+    data = {
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA,NY",
+        "description": "python",
+        "date_posted": "2022-03-20"
+        }
+    response = client.post("/jobs/create-job/",json.dumps(data))
+    assert response.status_code == 200 
+    assert response.json()["company"] == "doogle"
+    assert response.json()["description"] == "python"
+
+
+def test_read_job(client): 
+    data = {
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA,NY",
+        "description": "python",
+        "date_posted": "2022-03-20"
+        }
+    response = client.post("/jobs/create-job/",json.dumps(data))
+
+    response = client.get("/jobs/get/1/")
+    assert response.status_code == 200
+    assert response.json()['title'] == "SDE super"
+
+
+def test_job_not_found(client):
+    response = client.get("/jobs/get/2/")
+    assert response.status_code == 404
+    assert response.json()['detail'] == "Job with this id 2 does not exist"
